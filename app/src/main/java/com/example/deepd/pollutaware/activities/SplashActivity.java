9a82d0d0f2f25317db1ca35817666092c28abe7a
Sujoy Datta <sujoydatta26@gmail.com>
2018-10-27 13:00:00 +0530

    packages created

---
+++ app/src/main/java/com/example/deepd/pollutaware/activities/SplashActivity.java
@@ -0,0 +1,25 @@
+/*
+ * Created by Sujoy Datta. Copyright (c) 2018. All rights reserved.
+ *
+ * To the person who is reading this..
+ * When you finally understand how this works, please do explain it to me too at sujoydatta26@gmail.com
+ * P.S.: In case you are planning to use this without mentioning me, you will be met with mean judgemental looks and sarcastic comments.
+ */
+
+package com.example.deepd.pollutaware.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.deepd.pollutaware.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+    // useless
+}
